faf1ef6bcf1a52b058868b7e99e4f12694171ac0
SzymonSAP <szymon.pietraszek@sap.com>
2025-01-22 11:10:34 +0100

    Remove changing of namespace from config changes

--- pkg/metal/create_machine.go
+++ pkg/metal/create_machine.go
@@ -172,10 +172,6 @@ func (d *metalDriver) applyIgnition(ctx context.Context, req *driver.CreateMachi
 		return nil, status.Error(codes.Internal, fmt.Sprintf("failed to create ignition file for machine %s: %v", req.Machine.Name, err))
 	}
 
-	d.clientProvider.Lock()
-	providerNamespace := d.clientProvider.Namespace
-	d.clientProvider.Unlock()
-
 	ignitionData := map[string][]byte{}
 	ignitionData["ignition"] = []byte(ignitionContent)
 	ignitionSecret := &corev1.Secret{
@@ -185,7 +181,7 @@ func (d *metalDriver) applyIgnition(ctx context.Context, req *driver.CreateMachi
 		},
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      d.getIgnitionNameForMachine(ctx, req.Machine.Name),
-			Namespace: providerNamespace,
+			Namespace: d.metalNamespace,
 		},
 		Data: ignitionData,
 	}
@@ -202,7 +198,7 @@ func (d *metalDriver) applyServerClaim(ctx context.Context, req *driver.CreateMa
 		},
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      req.Machine.Name,
-			Namespace: providerNamespace,
+			Namespace: d.metalNamespace,
 			Labels:    providerSpec.Labels,
 		},
 		Spec: metalv1alpha1.ServerClaimSpec{
